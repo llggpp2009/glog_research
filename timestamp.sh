642f1cfe5b2f38b86b7974c4cd59c36fbfd21832
zhenglin1 <zhenglin1@jd.com>
2015-09-15 15:24:10 +0800

    revise the timestamp.sh

--- timestamp.sh
+++ timestamp.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 for FILE in $(git ls-files)
 do
 #FILE=aclocal.m4
-    TIME=$(git log --pretty=format:%cd -n 1 --date=iso $FILE)
-    #TIME=$(date -s '%Y-%m-%d %H:%M:%S %z' "$TIME" +%Y%m%d%H%M.%S)
-    #echo $TIME
-#    TIME=$("$TIME" +%Y%m%d%H%M.%S)
-    touch -hd $TIME $FILE
+    TIME=$(git log --pretty=format:%cd -n 1 --date=iso $FILE)    
+   TIME=$(date -d "$TIME" +%Y%m%d%H%M)   
+   touch -ht $TIME $FILE
 done
 
