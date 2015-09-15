0aa1dc2036f7c07f1c89fba62bb0f9bb92dfb507
liuguoping <liuguoping@jd.com>
2015-09-15 11:11:31 +0800

    delete echo

--- timestamp.sh
+++ timestamp.sh
@@ -5,7 +5,7 @@ do
 #FILE=aclocal.m4
     TIME=$(git log --pretty=format:%cd -n 1 --date=iso $FILE)
     #TIME=$(date -s '%Y-%m-%d %H:%M:%S %z' "$TIME" +%Y%m%d%H%M.%S)
-    echo $TIME
+    #echo $TIME
 #    TIME=$("$TIME" +%Y%m%d%H%M.%S)
     touch -hd $TIME $FILE
 done
